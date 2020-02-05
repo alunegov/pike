d8d9d7dbd0b1141644476da8a3056cf2b2b4eeaf
Alexander Lunegov <alunegov@gmail.com>
2020-02-05 16:31:49 +0500

    test(dc/lcard): тестирование AdcRead (на железе)

--- dc/lcard/LCardDaqTest.cpp
+++ dc/lcard/LCardDaqTest.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <fstream>
 #include <future>
 #include <thread>
 
@@ -6,6 +7,9 @@
 
 #include <LCardDaq.h>
 
+#include <RosMath.h>
+
+// ƒружественный к LCardDaq класс
 class LCardDaqTest
 {
 public:
@@ -22,7 +26,156 @@ private:
     ros::dc::lcard::LCardDaq& _daq;
 };
 
-TEST_CASE("LCardDaq", "[LCardDaq]") {
+template <typename T,
+    std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+constexpr bool double_equals(T a, T b, double_t eps = 0.01)
+{
+    return std::abs(a - b) < eps;
+}
+
+// экспорт сигнала в файл в формате dsv
+template <typename T>
+void ExportToDsv(const std::vector<T>& values, double_t dx, const std::string& filename)
+{
+    std::ofstream file{filename};
+
+    for (size_t i = 0; i < values.size(); ++i) {
+        file << i * dx << ";" << values[i] << std::endl;
+    }
+}
+
+static_assert(sizeof(int16_t) == sizeof(AdcRaw_t), "");
+
+using Harmonic = std::tuple<double_t, double_t, ptrdiff_t>;
+
+// поиск максимальной гармоники в спектре, полученном из сигнала канала
+Harmonic FindMaxHarmonic(const std::vector<int16_t>& values, double_t df)
+{
+    assert(!values.empty());
+    assert(df > 0);
+
+    const size_t points_count{values.size()};
+
+    const auto channel_mean = Mean_AdcRaw(values.data(), static_cast<uint32_t>(points_count));
+
+    std::vector<double_t> ampls(points_count);
+    std::vector<double_t> phases(points_count);
+
+    if (!Rfft(values.data(), ampls.data(), phases.data(), static_cast<uint32_t>(points_count), channel_mean, false)) {
+        return std::make_tuple(0.0, 0.0, -1);
+    }
+
+    // дальше работаем не со всем спектром, а только с его первой половиной (он симметричен)
+    const size_t spec_points_count{points_count / 2};
+
+    Cmplx_Ampl(ampls.data(), phases.data(), static_cast<uint32_t>(points_count), static_cast<uint32_t>(spec_points_count));
+
+    // zero DC
+    ampls[0] = 0.0;
+
+    const auto ampls_end{ampls.begin() + spec_points_count};
+    const auto max_ampl_it = std::max_element(ampls.begin(), ampls_end);
+    if (max_ampl_it == ampls_end) {
+        return std::make_tuple(0.0, 0.0, -1);
+    }
+
+    const auto max_ampl_index = std::distance(ampls.begin(), max_ampl_it);
+
+    assert(max_ampl_index >= 0);
+    const auto max_ampl_freq = IndexToFrequency(static_cast<uint32_t>(max_ampl_index), df);
+
+    return std::make_tuple(*max_ampl_it, max_ampl_freq, max_ampl_index);
+}
+
+// поиск максимальной гармоники в спектре, полученном из сигнала указанного канала (из покадровых данных от ј÷ѕ)
+Harmonic FindMaxHarmonicInChannel(const std::vector<int16_t>& values, size_t points_count, size_t channels_count,
+        size_t channel_index, double_t reg_freq)
+{
+    assert(values.size() >= points_count * channels_count);
+    assert(points_count > 0);
+    assert(channels_count > 0);
+    assert(channel_index <= channels_count);
+    assert(reg_freq > 0);
+
+    std::vector<int16_t> channel_values(points_count);
+
+    ExtractChannelFromAdcFrames(values.data(), channel_values.data(), points_count, channels_count, channel_index);
+
+    const auto dt{1 / (reg_freq * 1000)};
+    const auto df{1 / (dt * points_count)};
+
+    //ExportToDsv(channel_values, dt, "");
+
+    return FindMaxHarmonic(channel_values, df);
+}
+
+const auto RefRegFreq{12.8};
+
+TEST_CASE("LCardDaq AdcRead", "[LCardDaq]") {
+    ros::dc::lcard::LCardDaq sut;
+
+    // Init
+    const auto init_opt = sut.Init(0);
+
+    REQUIRE(init_opt);
+
+    SECTION("sync mode") {
+        double_t reg_freq{RefRegFreq};
+        const size_t points_count{1024};
+        const ros::dc::DAQ::_Channels channels{0, 1, 2, 3};
+        std::vector<int16_t> vals(points_count * channels.size());
+
+        const auto adc_read_opt = sut.AdcRead(reg_freq, points_count, channels, vals.data());
+
+        REQUIRE(adc_read_opt);
+        REQUIRE(double_equals(reg_freq, RefRegFreq, 0.03));
+
+        const size_t test_points_count{1024};
+        const auto max_harm = FindMaxHarmonicInChannel(vals, test_points_count, channels.size(), 0, reg_freq);
+
+        REQUIRE(std::get<2>(max_harm) != -1);
+        REQUIRE(double_equals(std::get<1>(max_harm), 262.0, 1.0));
+    }
+
+    SECTION("async mode") {
+        double_t reg_freq{RefRegFreq};
+        const ros::dc::DAQ::_Channels channels{0, 1, 2, 3};
+        std::atomic_bool cancel_token{false};
+
+        size_t callback_counter{0};
+        std::vector<int16_t> vals;
+
+        auto adc_read_f = std::async(std::launch::async, [&]() {
+            const auto adc_read_callback = [&](const int16_t* values, size_t values_count) {
+                callback_counter++;
+
+                vals.insert(vals.end(), values, values + values_count);
+            };
+
+            return sut.AdcRead(reg_freq, channels, cancel_token, adc_read_callback);
+        });
+
+        std::this_thread::sleep_for(std::chrono::seconds{3});
+        cancel_token = true;
+
+        const auto adc_read_opt = adc_read_f.get();
+
+        REQUIRE(adc_read_opt);
+        REQUIRE(double_equals(reg_freq, RefRegFreq, 0.03));
+        REQUIRE(callback_counter > 0);
+        REQUIRE(vals.size() > 0);
+
+        const size_t test_points_count{1024};
+        const auto max_harm = FindMaxHarmonicInChannel(vals, test_points_count, channels.size(), 0, reg_freq);
+
+        REQUIRE(std::get<2>(max_harm) != -1);
+        REQUIRE(double_equals(std::get<1>(max_harm), 262.0, 1.0));
+    }
+
+    sut.Deinit();
+}
+
+TEST_CASE("LCardDaq PrepareAdc", "[LCardDaq]") {
     ros::dc::lcard::LCardDaq sut;
     LCardDaqTest sut_test{sut};
 
@@ -31,7 +184,7 @@ TEST_CASE("LCardDaq", "[LCardDaq]") {
 
     REQUIRE(init_opt);
 
-    double_t reg_freq{12.8};
+    double_t reg_freq{RefRegFreq};
     const ros::dc::DAQ::_Channels channels{0, 1, 2, 3};
     size_t half_buffer{0};
     void* data{nullptr};
@@ -41,31 +194,10 @@ TEST_CASE("LCardDaq", "[LCardDaq]") {
     const auto prepare_adc_res = sut_test.PrepareAdc(reg_freq, channels, &half_buffer, &data, &sync);
 
     REQUIRE(prepare_adc_res == L_SUCCESS);
-    REQUIRE(abs(reg_freq - 12.8) < 0.03);
+    REQUIRE(double_equals(reg_freq, RefRegFreq, 0.03));
     REQUIRE(half_buffer > 0);
     REQUIRE(data != nullptr);
     REQUIRE(sync != nullptr);
 
-    // AdcRead
-    std::atomic_bool cancel_token{false};
-
-    auto adc_read_f = std::async(std::launch::async, [&sut, &reg_freq, &channels, &cancel_token]() {
-        size_t callback_counter{0};
-        const auto adc_read_callback = [&callback_counter](const int16_t* values, size_t values_count) {
-            callback_counter++;
-        };
-
-        const auto adc_read_opt = sut.AdcRead(reg_freq, channels, cancel_token, adc_read_callback);
-
-        return std::make_pair(adc_read_opt, callback_counter);
-    });
-
-    std::this_thread::sleep_for(std::chrono::seconds{3});
-    cancel_token = true;
-
-    const auto adc_read_res = adc_read_f.get();
-
-    REQUIRE(adc_read_res.first);
-    REQUIRE(adc_read_res.second > 0);
-    REQUIRE(abs(reg_freq - 12.8) < 0.03);
+    sut.Deinit();
 }
